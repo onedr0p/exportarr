aebd0de203f8302532b0fce41888b7ba08179d6d
kinduff <kinduff@protonmail.com>
2021-03-17 15:16:17 +0100

    Uses Exportarr name for index

--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -6,6 +6,6 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, _ *http.Request) {
-	response := `<h1>Radarr Exporter</h1><p><a href='/metrics'>metrics</a></p>`
+	response := `<h1>Exportarr</h1><p><a href='/metrics'>metrics</a></p>`
 	fmt.Fprintf(w, response)
 }
