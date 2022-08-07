bab8c94e141aeeb94bb56cededfe8274332e594d
Gavin Mogan <git@gavinmogan.com>
2022-08-07 14:33:43 -0700

    Super basic prowlarr support - #35

--- internal/model/config.go
+++ internal/model/config.go
@@ -5,8 +5,15 @@ import (
 )
 
 type Config struct {
-	XMLName xml.Name `xml:"Config"`
-	ApiKey  string   `xml:"ApiKey"`
-	Port    string   `xml:"Port"`
-	UrlBase string   `xml:"UrlBase"`
+	XMLName    xml.Name `xml:"Config"`
+	ApiKey     string   `xml:"ApiKey"`
+	Port       string   `xml:"Port"`
+	UrlBase    string   `xml:"UrlBase"`
+	ApiVersion string   `ml:"ApiVersion"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		ApiVersion: "v3",
+	}
 }
