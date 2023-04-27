a42b0cb8487ae9fba34fb4f602045419bc2fbd90
Russell Troxel <russelltroxel@gmail.com>
2023-04-27 13:56:49 -0700

    Fix #154: Do not require Api Key or URL in parent config, validate in child config (#157)
    Signed-off-by: Russell Troxel <russell@troxel.io>

---
+++ internal/arr/config/arr_integration_test.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"testing"
+
+	base_config "github.com/onedr0p/exportarr/internal/config"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/require"
+)
+
+func TestArrConfigIntegration(t *testing.T) {
+	require := require.New(t)
+
+	configFlags := pflag.NewFlagSet("test", pflag.ContinueOnError)
+	base_config.RegisterConfigFlags(configFlags)
+
+	arrConfigFlags := testFlagSet()
+
+	t.Setenv("URL", "http://localhost")
+	t.Setenv("CONFIG", "test_fixtures/config.test_xml")
+	t.Setenv("PORT", "9792")
+	t.Setenv("ENABLE_ADDITIONAL_METRICS", "true")
+	t.Setenv("ENABLE_UNKNOWN_QUEUE_ITEMS", "false")
+
+	config, err := base_config.LoadConfig(configFlags)
+	require.NoError(err)
+	arrConfig, err := LoadArrConfig(*config, arrConfigFlags)
+	require.NoError(err)
+
+	require.NoError(config.Validate())
+	require.NoError(arrConfig.Validate())
+
+}
