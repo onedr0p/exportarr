c7ce4675084313cb4e0932ac7233953a4d19d55d
Russell Troxel <russelltroxel@gmail.com>
2023-06-07 14:21:35 -0700

    Add AppInfo metric & automatic build time app info population (#178)

--- cmd/exportarr/main.go
+++ cmd/exportarr/main.go
@@ -2,6 +2,18 @@ package main
 
 import "github.com/onedr0p/exportarr/internal/commands"
 
+var (
+	appName   = "exportarr"
+	version   = "development"
+	buildTime = ""
+	revision  = ""
+)
+
 func main() {
-	commands.Execute()
+	commands.Execute(commands.AppInfo{
+		Name:      appName,
+		Version:   version,
+		BuildTime: buildTime,
+		Revision:  revision,
+	})
 }
